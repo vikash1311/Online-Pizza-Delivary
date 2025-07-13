a52477c7cb33d2d1d5c2e2eb910b97499bec664f
vikash1311 <151333397+vikash1311@users.noreply.github.com>
2025-07-13 19:32:19 +0530

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2022 Designed by <a href="https://github.com/AniketBichare" target="_blank" rel="noopener noreferrer">@AniketBichare</a></p>
+</div>
